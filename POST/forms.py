311aca122151f11339bbbe2a241a81075c848628
Sian Jones <sianwroejones@gmail.com>
2024-07-08 21:27:26 +0000

    forms styling with bootstrap

---
+++ POST/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'description')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
