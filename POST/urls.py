101a54d7416a059371f0598125a378582db050c9
Sian Jones <sianwroejones@gmail.com>
2024-07-08 18:49:17 +0000

    add_post html with form

--- POST/urls.py
+++ POST/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, PostDetailView
+from .views import HomeView, PostDetailView, AddPostView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
     path('details/<int:pk>', PostDetailView.as_view(), name='details'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
--- POST/views.py
+++ POST/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 # Create your views here.
 
@@ -13,3 +13,8 @@ class HomeView(ListView):
 class PostDetailView(DetailView):
     model = Post 
     template_name = 'post_details.html'  
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
