77bfea746a1c632ae84a52489dd56c463ef75a10
Sian Jones <sianwroejones@gmail.com>
2024-07-08 15:20:09 +0000

    detail view, templete etc

--- POST/urls.py
+++ POST/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from . import views
-from .views import HomeView
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
+    path('details/<int:pk>', PostDetailView.as_view(), name='details'),
 ]
