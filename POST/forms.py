67d51803ca704bf20bfb0735e0defdb43ced009c
Sian Jones <sianwroejones@gmail.com>
2024-07-10 20:44:35 +0000

    added some javascript to forms.py to dtermine who author is when they post

--- POST/forms.py
+++ POST/forms.py
@@ -19,7 +19,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control', 'placeholder':'user name'}),
             'medium': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
