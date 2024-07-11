00642d2333822e25518948af2823ca69d6f0b461
Sian Jones <sianwroejones@gmail.com>
2024-07-11 10:00:02 +0000

    migrated new comments model

--- POST/admin.py
+++ POST/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Medium
+from .models import Post, Medium, Comment
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Medium)
+admin.site.register(Comment)
