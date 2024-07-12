402a17b3f5283bc4a97dbeb7f4179810ef1c265b
Sian Jones <sianwroejones@gmail.com>
2024-07-12 21:03:39 +0000

    medium dropdown menu in navbar in every view

--- POST/views.py
+++ POST/views.py
@@ -13,10 +13,23 @@ class HomeView(ListView):
     template_name = 'home.html'
     #ordering = ['-id']
     ordering = ["-post_date"]
+    meds = Medium.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        meds_menu = Medium.objects.all()   
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["meds_menu"] = meds_menu
+        return context
 
 class PostDetailView(DetailView):
     model = Post 
     template_name = 'post_details.html'  
+
+    def get_context_data(self, *args, **kwargs):
+        meds_menu = Medium.objects.all()   
+        context = super(PostDetailView, self).get_context_data(*args, **kwargs)
+        context["meds_menu"] = meds_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
@@ -25,12 +38,24 @@ class AddPostView(CreateView):
     #fields = '__all__'
     #fields = ('title', 'description',)
 
+    def get_context_data(self, *args, **kwargs):
+        meds_menu = Medium.objects.all()   
+        context = super(AddPostView, self).get_context_data(*args, **kwargs)
+        context["meds_menu"] = meds_menu
+        return context
+
 class AddCommentView(CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'add_comment.html'
     #fields = '__all__'
     #fields = ('title', 'description',)
+
+    def get_context_data(self, *args, **kwargs):
+        meds_menu = Medium.objects.all()   
+        context = super(AddCommentView, self).get_context_data(*args, **kwargs)
+        context["meds_menu"] = meds_menu
+        return context
 
     def form_valid(self, form):
         form.instance.post_id = self.kwargs['pk']
@@ -45,23 +70,38 @@ class AddMediumView(CreateView):
         fields = '__all__'
         #fields = ('title', 'description',)
 
+        def get_context_data(self, *args, **kwargs):
+            meds_menu = Medium.objects.all()   
+            context = super(AddMediumView, self).get_context_data(*args, **kwargs)
+            context["meds_menu"] = meds_menu
+            return context
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
     #fields = ['title', 'title_tag', 'description', 'medium',]
 
+def get_context_data(self, *args, **kwargs):
+            meds_menu = Medium.objects.all()   
+            context = super(UpdatepostView, self).get_context_data(*args, **kwargs)
+            context["meds_menu"] = meds_menu
+            return context
+        
+
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
 
-def MediumView(request, meds):
-    medium_posts = Post.objects.filter(medium=meds.replace('-', ' '))
-    return render(request, 'medium.html', {'meds':meds, 'medium_posts':medium_posts})
 
 def get_context_data(self, *args, **kwargs):
-    meds_menu = Medium.objects.all()   
-    context = super(HomeView, self).get_context_data(*args, **kwargs)
-    context["meds_menu"] = meds_menu
-    return context
+            meds_menu = Medium.objects.all()   
+            context = super(DeletePostView, self).get_context_data(*args, **kwargs)
+            context["meds_menu"] = meds_menu
+            return context
+
+def MediumView(request, meds):
+    medium_posts = Post.objects.filter(medium=meds.replace('-', ' '))
+    return render(request, 'medium.html', {'meds':meds.title(), 'medium_posts':medium_posts})
+
