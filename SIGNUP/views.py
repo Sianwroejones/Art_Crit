46e7ca2db6928cfbb95ba97bbbfb9b2ea286ec53
Sian Jones <sianwroejones@gmail.com>
2024-07-13 20:46:05 +0000

    notification for signup

--- SIGNUP/views.py
+++ SIGNUP/views.py
@@ -3,6 +3,8 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 # Create your views here.
 
@@ -10,3 +12,11 @@ class UserRegistrationView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+    success_message = "Your account was created successfully. Please log in."
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, self.success_message)
+        return response
+
+
