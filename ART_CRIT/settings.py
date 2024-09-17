35d6bd23fbabaded41309b0664e5baea32d1b59e
Sian Jones <sianwroejones@gmail.com>
2024-09-17 19:27:05 +0000

    reinatalled text editor

--- ART_CRIT/settings.py
+++ ART_CRIT/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sianwroejones-artcrit-ymhmmovszvt.ws.codeinstitute-ide.net','.herokuapp.com',]
+ALLOWED_HOSTS = ['8000-sianwroejones-artcrit-ymhmmovszvt.ws.codeinstitute-ide.net','.herokuapp.com','8000-sianwroejones-artcrit-njbr4gaty1h.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'cloudinary_storage', 
     'cloudinary',
     'SIGNUP',
-    'ckeditor',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
