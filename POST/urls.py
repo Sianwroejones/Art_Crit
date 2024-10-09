733e8654cfbd15536cf602c1d950260f1e1d9b5a
Sian Jones <sianwroejones@gmail.com>
2024-10-09 20:45:06 +0000

    corrected python validaton error messages for urls.py

--- POST/urls.py
+++ POST/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
 # from . import views
-from .views import (SplashView, HomeView, PostDetailView, AddPostView, UpdatePostView, DeletePostView, AddMediumView, MediumView, AddCommentView)
+from .views import (
+    SplashView, HomeView, PostDetailView, AddPostView,
+    UpdatePostView, DeletePostView, AddMediumView,
+    MediumView, AddCommentView)
 from POST.views import SplashView
 
 urlpatterns = [
-    #path('', views.home, name="home"),
     path('', SplashView.as_view(), name="splash"),
     path('home/', HomeView.as_view(), name="home"),
     path('details/<int:pk>', PostDetailView.as_view(), name='details'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('add_medium/', AddMediumView.as_view(), name='add_medium'),
-    path('details/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'), 
+    path(
+        'details/edit/<int:pk>',
+        UpdatePostView.as_view(),
+        name='update_post'
+    ),
     path('details/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'), 
     path('medium/<str:meds>/', MediumView, name='medium'),
     path('details/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
