714967b379d6a0365f41695d60cb3278eef42c44
Sian Jones <sianwroejones@gmail.com>
2024-10-10 17:03:33 +0000

    fixed all python errors

--- POST/forms.py
+++ POST/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from .models import Post, Medium, Comment
 
-#choices = [('Painting', 'Painting'), ('Print', 'Print'), ('Drawing', 'Drawing'),
-#('Ceramics', 'Ceramics'), ('Mixed Media', 'Mixed Media'), ('Sculpture', 'Sculpture'), 
-#('Wood', 'Wood'), ('Collage', 'Collage'), ('Photography', 'Photography'), ('Other', 'Other'),]
 choices = Medium.objects.all().values_list('type', 'type')
 
 choice_list = []
@@ -11,19 +8,35 @@ choice_list = []
 for item in choices:
     choice_list.append(item)
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'medium', 'description', 'header_image')
+        fields = (
+            'title', 'title_tag', 'author', 'medium',
+            'description', 'header_image'
+            )
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
-            #'author': forms.Select(attrs={'class': 'form-control', 'placeholder':'user name'}),
-            'medium': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+            'author': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'value': '',
+                    'id': 'user',
+                    'type': 'hidden'
+                }
+            ),
+            'medium': forms.Select(
+                choices=choice_list,
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
 
 class EditForm(forms.ModelForm):
     class Meta:
@@ -31,11 +44,16 @@ class EditForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'description')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type title of artwork here'}),
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Type title of artwork here'
+                }
+            ),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            #'medium': forms.Select(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
-        }       
+        }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -45,4 +63,4 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
-        }       
+        }
